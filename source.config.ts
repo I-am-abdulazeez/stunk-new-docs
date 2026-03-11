40ad4011e95b5477985097280d7e2e5b768a110b
AbdulAzeez <abdulazeezolamide50@gmail.com>
2026-03-11 17:01:52 +0100

    it's what Vite and Anthony Fu's projects use

--- source.config.ts
+++ source.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig, defineDocs } from 'fumadocs-mdx/config';
+import { rehypeCode, } from 'fumadocs-core/mdx-plugins';
 
 export const docs = defineDocs({
   dir: 'content/docs',
 });
 
-export default defineConfig();
+export default defineConfig({
+  mdxOptions: {
+    rehypePlugins: [
+      [rehypeCode, {
+        themes: {
+          light: 'rose-pine-dawn',
+          dark: 'rose-pine',
+        }
+      },]
+    ]
+  }
+});
